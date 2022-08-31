65419e402b0bb66d80aacd556c400dad0782b825
bindjeme <bindjemeb@yahoo.fr>
2022-08-31 15:05:45 +0200

    mise en place du load balancing Feign et Eureka de spring cloud

--- currency-conversion-service/src/main/java/fr/sang/microservice/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/fr/sang/microservice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import fr.sang.microservice.currencyconversionservice.controllers.dto.CurrencyConversion;
 
 //info du service qu' on veut appeler 
-@FeignClient(name = "currency-exchange", url = "localhost:8081")
+//@FeignClient(name = "currency-exchange", url = "localhost:8081")
+//le service  parlera à eureka pour connaitre  les  instances disponibles 
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
